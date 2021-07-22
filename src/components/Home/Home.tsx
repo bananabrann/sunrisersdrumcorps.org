9bdb36845eeefa8874a4264332c11b1312e57451
bananabrann <plbrannan95@gmail.com>
2021-07-22 18:17:32 -0400

    Finish bonus area styling and associated defects

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -19,6 +19,13 @@ const Home: React.FC<IHomeProps> = (props: IHomeProps) => {
           <p>
             Bonus area is great for including snippets of stuff. Though
             preferably text, this can ulatimately be anything you would like.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            voluptatem incidunt dignissimos non debitis voluptate vel, impedit
+            ex eum necessitatibus? Voluptatibus et dolor voluptate aspernatur
+            iusto nam, tempora corrupti repellendus. Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Aliquid eaque, dolorum, sed
+            perferendis nulla obcaecati iste magni molestias neque repellendus
+            voluptas ut itaque sit. Ipsum nam impedit ut reiciendis autem?
           </p>
           <div className="do-more">Do More!</div>
         </div>
