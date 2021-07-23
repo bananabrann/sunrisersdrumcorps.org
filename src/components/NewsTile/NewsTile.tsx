578d0c2d6c32c1966a262b7b438d2bb5758f051b
bananabrann <plbrannan95@gmail.com>
2021-07-23 18:45:24 -0400

    Add base footer

--- src/components/NewsTile/NewsTile.tsx
+++ src/components/NewsTile/NewsTile.tsx
@@ -3,8 +3,6 @@ import dateFormat from "dateformat";
 import pagnateText from "../../utils";
 import "./NewsTile.scss";
 
-const NUM_TRIM_TEXT_BY: number = 40; 
-
 interface INewsTileProps {
   title: string;
   body: string;
@@ -17,8 +15,8 @@ const NewsTile: React.FC<INewsTileProps> = (props: INewsTileProps) => {
   const [date, setDate] = useState<Date>(new Date(props.date));
 
   useEffect(() => {
-    
-  }, [])
+    if(typeof props.date === "string") setDate(new Date(props.date));
+  }, [props.date]);
 
   return(
     <div className="NewsTile">
@@ -26,7 +24,7 @@ const NewsTile: React.FC<INewsTileProps> = (props: INewsTileProps) => {
         <p className="title">{pagnateText(props.title, 30)}</p>
         <i className="fas fa-external-link-alt"></i>
       </div>
-      <p className="date">{dateFormat(props.date, "dddd, mmmm dS, yyyy")}</p>
+      <p className="date">{dateFormat(date, "dddd, mmmm dS, yyyy")}</p>
       <p className="body">{pagnateText(props.body, 180)}</p>
     </div>
   )
