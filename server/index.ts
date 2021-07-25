469fe3854c945677eb9b00875434b82d5c4c97d5
bananabrann <plbrannan95@gmail.com>
2021-07-25 13:38:26 -0400

    Add condition for 404 pages

--- server/index.ts
+++ server/index.ts
@@ -6,8 +6,11 @@ const app: express.Application = express();
 app.get("/", (req, res) => {
   res.status(200).send("API is ready.");
 });
-
 app.use("/api/news", newsRoute);
+
+app.get("*", (req: express.Request, res: express.Response): void => {
+  res.status(404).send({ message: "Huh, we couldn't find that page..." });
+});
 
 const PORT: string | number = process.env.PORT || 8000;
 app.listen(PORT, () => {
