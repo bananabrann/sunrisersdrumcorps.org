9daf467b65fda22d0d36bba6fc8175a51ca179fa
Lee <plbrannan95@gmail.com>
2021-11-27 13:37:01 -0500

    Remove unused server, add iac

--- server/routes/news.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const NewsController = require("../controllers/news.ts");
-
-router.get("/", NewsController.getAllNews);
-// router.get("/:slug", NewsController.getSingleNewsBySlug);
-
-module.exports = router;
