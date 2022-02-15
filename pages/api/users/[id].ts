97bbd832e1f6f48fa12ea97741e8f7b3c9f7bd87
bananabrann <bananabrann@users.noreply.github.com>
2022-02-15 00:01:27 +0000

    Prettified Code!

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function getUserById(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: `Got id ${req.query.id}` })
-} 
+  res.status(200).json({ message: `Got id ${req.query.id}` });
+}
